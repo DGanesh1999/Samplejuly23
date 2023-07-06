b6105678cfbcf2e97caa3cb6d62bd23bd6666f98
DGanesh1999 <ganesh1999gand@gmail.com>
2023-07-06 20:51:49 +0530

    Sample1

---
+++ src/org/login/Employee.java
@@ -0,0 +1,15 @@
+package org.login;
+
+public class Employee {
+	private void username() {
+		System.out.println("Username is Ganesh");
+
+	}
+	public static void main(String[] args) {
+		
+	
+	Employee e= new Employee();
+	e.username();
+	
+}
+}
